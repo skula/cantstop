f8f3dabfc4e7f9742fff7e6322bfc074f6241339
Stephane <Stephane@192.168.1.22>
2015-09-22 19:08:55 +0200

    first init

---
+++ Can'tStop/src/com/skula/cantstop/activities/views/BoardView.java
@@ -0,0 +1,39 @@
+package com.skula.cantstop.activities.views;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class BoardView extends View {
+	private Paint paint;
+	private Resources res;
+
+	public BoardView(Context context) {
+		super(context);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int x = (int) event.getX();
+		int y = (int) event.getY();
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			break;
+		case MotionEvent.ACTION_MOVE:
+			break;
+		case MotionEvent.ACTION_UP:
+			break;
+		}
+		invalidate();
+		return true;
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+
+	}
+}
